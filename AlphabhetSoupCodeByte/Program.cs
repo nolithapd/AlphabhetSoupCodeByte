7f8f1c59a6585a6aa85b0bb1458343689c72736e
Nolitha Portia Dyani <portia.dyani@redpandasoftware.co.za>
2023-08-14 14:06:23 +0200

    first commit

---
+++ AlphabhetSoupCodeByte/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AlphabhetSoupCodeByte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testString = "hello";
+            string alphabetSoupResults = AphabhetSoup(testString);
+            Console.WriteLine(alphabetSoupResults);
+        }
+        /**
+         * Have the function alphabetSoup(str) take the str string parameter being
+         * passed and return the string with the letters in alphabetical order (ie.
+         * hello becomes ehllo). Assume numbers and punctuation symbols will not be
+         * included in the string.
+         */
+        public static string AphabhetSoup(string strTest)
+        {
+            char[] characters = strTest.ToCharArray();
+            Array.Sort(characters);
+            return new string(characters);
+
+        }
+    }
+ }
